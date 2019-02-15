051bb27b5de170f861cdc9d64450e28e54673725
Maxim <yoloplayerr@users.noreply.github.com>
2019-02-16 01:23:40 +0300

    commit

--- student_project/src/economic_project/main.java
+++ student_project/src/economic_project/main.java
@@ -1,25 +1,22 @@
 package economic_project;
 
+import java.util.ArrayList;
+
 public class main {
 
-	public static int mnk(String[] text) {
+	public static double mnk(ArrayList<String> text) {
+		ArrayList<Double> be=new ArrayList<>();
+		double y=0;	
+			
+			for (int i=0;i<text.size();i++) {
+				be.add(Double.parseDouble(text.get(i)));
+			}
+	       for (int i=0;i<text.size();i++) {
+	    	y+=be.get(i);
+	       }
+	      
 		
-	       
-	      double[] t= {1,2,3,4,5,6,7,8,9};
-	      double[] y= {141.5000000,136.4000000,135.9900000,130.6000000,133.4900000,133.4000000,133.3400000,134.6900000,135.1000000};
-	      double y_middle = 0;
-	      double t2=0;
-//	      double ty=0;
-//	      int t=0;
-//	      
-//	      for (int i=1;i<y.length+1;i++) {
-//	    	  y_middle +=y[i];
-//	    	  t2+=i*i;
-//	    	  t+=i;
-//	    	  
-//	      }
-		return (int) t2;
-	       
+	       return y;
 	    }	
     
 	}
--- student_project/src/economic_project/reader1.java
+++ student_project/src/economic_project/reader1.java
@@ -7,52 +7,44 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class reader1 {
 
+	public static void main(String[] args) {
+		// Считывание данных в динамический массив
+		ArrayList<String> text = new ArrayList<>();
+		try {
+			String str = null;
+			BufferedReader br = new BufferedReader(new FileReader("/Users/maxim/eurusd.csv"));
+			while ((str = br.readLine()) != null) {
+				text.addAll(Arrays.asList(str.split(" ")));
+
+			}
+			br.close();
+		} catch (IOException exc) {
+			System.out.println("IO error!" + exc);// some
+		}
 	
 
-	public static void main(String[] args) {
-		String[] text = new String[0];
-		try {
-		    String str = null;
-		    BufferedReader br = new BufferedReader(new FileReader("/Users/maxim/eurusd.csv"));
-		    while ((str = br.readLine()) != null) {
-		        //получаем новые слова
-		        String[] newWords = str.split(" ");
-		        //создаем расширенный массив
-		        String[] result = new String[text.length + newWords.length];
-		        //копируем элементы в массив
-		        System.arraycopy(text, 0, result, 0, text.length);
-		        System.arraycopy(newWords, 0, result, text.length, newWords.length);
-		        //присваиваем результирующий массив текущему
-		        text = result;
-		    }
-		    br.close();
-		} catch (IOException exc) {
-		    System.out.println("IO error!" + exc);//some
-		}for (int i=0;i<text.length;i++) {
-		    System.out.println(text[i]);
-		    }
-		main.mnk(text);
-			String[] some=text;
-			try(FileOutputStream fos=new FileOutputStream("/Users/maxim/close.txt"))
-		        {
-				for (int i=0;i<some.length;i++) {
-		            // перевод строки в байты
-		            byte[] buffer = some[i].getBytes();
-		              
-		            fos.write(buffer, 0, buffer.length);
-				}
-		        }
-		        catch(IOException ex){
-		              
-		            System.out.println(ex.getMessage());
-		        }
-		        System.out.println("The file has been written");
+		System.out.println(main.mnk(text));
+		
+		
+		// Запись данных в файл
+		try (FileOutputStream fos = new FileOutputStream("/Users/maxim/close.txt")) {
+			for (int i = 0; i < text.size(); i++) {
+				// перевод строки в байты
+				byte[] buffer = text.get(i).getBytes();
 
-		       //kek
-	            
-	            
-}
+				fos.write(buffer, 0, buffer.length);
+			}
+		} catch (IOException ex) {
+
+			System.out.println(ex.getMessage());
+		}
+		System.out.println("The file has been written");
+
+		// kek
+
+	}
 }
